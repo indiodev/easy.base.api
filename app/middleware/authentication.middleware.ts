6b7b9d97b188753f4295f9c45b4991cb74fd4c11
Luan Mineu Costa <luanwebdev@gmail.com>
2024-09-19 00:29:46 -0300

    feat: add middleware from the other version

---
+++ app/middleware/authentication.middleware.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { ApplicationException } from "@exceptions/application.exception";
+import { Authentication } from "@util/authentication";
+
+export async function AuthenticationMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  const token = request.headers.authorization?.replace("Bearer", "").trim();
+
+  if (!token)
+    throw new ApplicationException({
+      message: "Acesso não autorizado.",
+      cause: "UNAUTHORIZED",
+      code: 401,
+    });
+
+  try {
+    const { sub } = verify(token, Authentication.JWT.SECRET) as {
+      sub: string;
+    };
+
+    request.user = {
+      id: sub,
+    };
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
