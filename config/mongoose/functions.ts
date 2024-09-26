4bf72e96c8f1dac757f0324d64c69e86a0a7bb60
Luan Mineu Costa <luanwebdev@gmail.com>
2024-09-25 22:35:11 -0300

    feat: function to handle dynamic models

--- config/mongoose/functions.ts
+++ config/mongoose/functions.ts
@@ -1,6 +1,31 @@
 
 import mongoose, { Schema, Document, Model } from 'mongoose';
+import slugify from 'slugify';
+import { Models } from './schema';
 
+
+
+export const generateCollectionName = async (tableName: string): Promise<string> => {
+  const slugifiedName = slugify(tableName, { lower: true });
+  const collectionSize = await Models.Table.countDocuments();
+  return `${slugifiedName}_${collectionSize}_data`;
+};
+
+
+export const getColumnDataType = (type: string): any => {
+  switch (type) {
+    case 'editablecell':
+      return "String";
+    case 'numbercell':
+      return "Number";
+    case 'booleancell':
+      return "Boolean";
+    case 'datecell':
+      return "Date";
+    default:
+      return "String";
+  }
+}
 
 export const createDynamicModel = (
     collectionName: string,
@@ -11,10 +36,12 @@ export const createDynamicModel = (
     if (mongoose.models[collectionName]) {
       return mongoose.models[collectionName] as Model<Document>;
     }
-  
+    
+    schemaDefinition._id = { type: mongoose.Schema.Types.ObjectId, auto: true };
+
     const schema = new Schema(schemaDefinition, { timestamps: true });
 
-    const model = mongoose.model<Document>(collectionName, schema);
+    const model = mongoose.model<Document>(collectionName, schema, collectionName);
 
     return model;
   };
