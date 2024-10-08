c3ca50c79df2dfbf18b87eaedd56e0ebc26e4bba
Luan Mineu Costa <luanwebdev@gmail.com>
2024-10-08 19:53:26 -0300

    feat: add relational mapping on create and update column

--- app/repositories/table.repository.ts
+++ app/repositories/table.repository.ts
@@ -46,6 +46,7 @@ export class TableRepository {
             [item.slug]: {
               type: getColumnDataType(item.type),
               required: item.config?.required || false,
+              ...(item.type === "RELATIONAL" && { ref: item.config.relation.collection })
             },
           }))
           .reduce((acc: any, curr: any) => ({ ...acc, ...curr }), {})
@@ -82,6 +83,7 @@ export class TableRepository {
             [item.slug]: {
               type: getColumnDataType(item.type),
               required: item.config?.required || false,
+              ...(item.type === "RELATIONAL" && { ref: item.config.relation.collection })
             },
           }))
           .reduce((acc: any, curr: any) => ({ ...acc, ...curr }), {})
