bd2ade8ec617c40201a150d20e8be405f8123855
indiodev <jhollyfer.fr@gmail.com>
2024-10-22 06:20:20 -0500

    feat: save layout

--- app/controllers/user/table/layout.controller.ts
+++ app/controllers/user/table/layout.controller.ts
@@ -2,14 +2,18 @@ import { Response } from "express";
 
 import { UserFactory } from "@factories/user.factory";
 import { CustomRequest } from "@middleware/authentication.middleware";
+import { UserTableUpdateValidator } from "@validators/user.validator";
 
-export async function List(
+export async function TableLayout(
   request: CustomRequest,
   response: Response,
 ): Promise<Response> {
-  console.log({ id: request?.user?.id });
-
   const factory = UserFactory();
-  const result = await factory.list();
+  const payload = UserTableUpdateValidator.parse({
+    ...request.body,
+    id: request?.user?.id,
+  });
+
+  const result = await factory.tableLayoutView(payload);
   return response.status(200).json(result);
 }
--- app/dto/user.dto.ts
+++ app/dto/user.dto.ts
@@ -2,8 +2,10 @@ import { z } from "zod";
 
 import {
   UserCreateValidator,
+  UserTableUpdateValidator,
   UserUpdateValidator,
 } from "@validators/user.validator";
 
 export type UserUpdate = z.infer<typeof UserUpdateValidator>;
 export type UserCreate = z.infer<typeof UserCreateValidator>;
+export type UserTableUpdate = z.infer<typeof UserTableUpdateValidator>;
