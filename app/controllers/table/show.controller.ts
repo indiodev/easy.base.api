8dcc6202fe494def1a1ae9968ed2541fe25e8696
indiodev <jhollyfer.fr@gmail.com>
2024-11-02 00:03:48 -0500

    feat: pagination tables

--- app/controllers/table/show.controller.ts
+++ app/controllers/table/show.controller.ts
@@ -7,50 +7,9 @@ export async function Show(
   response: Response,
 ): Promise<Response> {
   try {
-    console.log({ id: request.params.id }, "CHAMOU AQUI");
     const factory = TableFactory();
-
-    const query = request.query;
-<<<<<<< HEAD
-
-    const page = Number(request.query.page ?? 1);
-    const per_page = Number(request.query.per_page ?? 10);
-
-    delete query.per_page;
-    delete query.page;
-
-    const { meta, table } = await factory.show({
-      id: request.params.id,
-      per_page,
-=======
-    const limit = Number(request.query.limit ?? 10);
-    const page = Number(request.query.page ?? 1);
-
-    delete query.limit;
-    delete query.page;
-
-    console.log({
-      id: request.params.id,
-      limit,
->>>>>>> 0bfd694 (feat: pagination tables)
-      page,
-      ...query,
-    });
-
-<<<<<<< HEAD
-    return response.status(200).json({ data: table, meta });
-=======
-    const result = await factory.show({
-      id: request.params.id,
-      limit,
-      page,
-      ...query,
-    });
-
-    console.log(result);
-
+    const result = await factory.show(request.params.id);
     return response.status(200).json(result);
->>>>>>> 0bfd694 (feat: pagination tables)
   } catch (error) {
     console.error(error);
     return response.status(400).json(error);
